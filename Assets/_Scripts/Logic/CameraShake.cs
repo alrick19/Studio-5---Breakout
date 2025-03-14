6b4b4c48b951135a5415e9c0ac97a486beb58a96
Willg0115 <willg0115@outlook.com>
2025-03-13 21:58:01 -0400

    camera shake implemented on brick hit for levels 1 and 2.

---
+++ Assets/_Scripts/Logic/CameraShake.cs
@@ -0,0 +1,26 @@
+using DG.Tweening;
+
+public class CameraShake : SingletonMonoBehavior<CameraShake>
+{
+
+    protected override void Awake()
+    {
+        base.Awake();
+        if (transform.parent != null) 
+        {
+            transform.SetParent(null);
+        }
+    }
+    public static void Shake(float duration, float strength)
+    {
+        if (Instance != null) 
+        {
+            Instance.OnShake(duration, strength);
+        }
+    }   
+    private void OnShake(float duration, float strength)
+    {
+        transform.DOShakePosition(duration, strength);
+        transform.DOShakeRotation(duration, strength);
+    }
+}
